3c5dd39941a671e98cd7abaf9c7a2becd013aa72
Elsa Martinez <elsa.martinez.faucard@gmail.com>
2018-09-27 18:02:14 +0200

    HTML, CSS, JS files done-test

--- JS files/main.js
+++ JS files/main.js
@@ -7,6 +7,83 @@ function buildDom(html) {
 }
 
 function main() {
-  var splashScreen = null;
-  
+
+  var mainContainerElement = document.querySelector('#main-container');
+
+  // -- Splash
+  var splashElement = null;
+  var splashButton = null;
+
+  var handleSplashClick = function () {
+    destroySplash();
+    buildGame();
+  }
+
+  function buildSplash() {
+    splashElement = buildDom(`
+      <main class="splash container">
+        <h1 class="splash__title">Canvas epic game</h1>
+        <button>Start</button>
+      </main>
+    `)
+    mainContainerElement.appendChild(splashElement);
+    
+    splashButton = document.querySelector('button');
+    splashButton.addEventListener('click', handleSplashClick)
+
+  }
+  function destroySplash() {
+    splashButton.removeEventListener('click', handleSplashClick);
+    splashElement.remove();
+  }
+
+  // -- Game
+  var game = null;
+  var handleGameOver = function () {
+    destroyGame();
+    buildGameover(game.score);
+  };
+
+  function buildGame() {
+    game = new Game(mainContainerElement);
+    game.onOver(handleGameOver);
+  }
+  function destroyGame() {
+    game.destroy();
+  }
+
+  // -- Gameover
+  var gameoverElement = null;
+  var gameoverButton = null;
+
+  var handleGameoverClick = function () {
+    destroyGameover();
+    buildSplash();
+  }
+
+  function buildGameover(score) {
+    gameoverElement = buildDom(`
+      <main class="gameover container">
+        <h1>Game over</h1>
+        <p>Your score: <span class="score"></span></p>
+        <button>Restart</button>
+      </main>
+    `);
+    mainContainerElement.appendChild(gameoverElement);
+
+    gameoverButton = document.querySelector('button');
+    gameoverButton.addEventListener('click', handleGameoverClick);
+
+    var scoreElement = document.querySelector('.score');
+    scoreElement.innerText = score;
+  }
+
+  function destroyGameover() {
+    gameoverButton.removeEventListener('click', handleGameoverClick);
+    gameoverElement.remove();
+  }
+
+  buildSplash();
 }
+
+document.addEventListener('DOMContentLoaded', main);
