bd18d1c395bc152de00cba3dd08114fe90500113
Elsa Martinez <elsa.martinez.faucard@gmail.com>
2018-09-30 16:35:09 +0200

    canvas done2

--- JS files/character.js
+++ JS files/character.js
@@ -1,11 +1,18 @@
 'use strict';
 
-function character(canvas) {
+function Character(canvas) {
   var self = this;
 
-  self.x = 2;
-  self.y = 384;
+  self.x = 100;
+  self.y = 100;
   self.size = 60;
   self.ctx = canvas.getContext('2d')
 }
-character(canvas)
+
+Character.prototype.render = function () {
+  var self = this;
+  self.ctx = canvas.getContext('2d');
+  self.ctx.fillStyle = "black";
+  self.ctx.fillRect(self.x, self.y, self.size, self.size);
+  
+}
--- JS files/game.js
+++ JS files/game.js
@@ -1,24 +1,23 @@
 'use strict';
 
-function Game(parent) {
+function Game(parent){
   var self = this;
 
   self.parentElement = parent;
-  self.gameElement = null
-  self.onGameOverCallback = null;
-
+  self.gameElement = null;
+  
   self._init();
+
 }
 
 Game.prototype._init = function () {
   var self = this;
 
   self.gameElement = buildDom(`
-    <main class="game container">
-      <header class="game__header"> 
+    <main class = "game container">
+      <header class="game__header">
         <div class="score">
           <span class="label">Score:</span>
-          <span class="value"></span>
         </div>
       </header>
       <div class="game__canvas">
@@ -31,8 +30,7 @@ Game.prototype._init = function () {
   self.canvasParentElement = document.querySelector('.game__canvas');
   self.canvasElement = document.querySelector('.canvas');
 
-  self.livesElement = self.gameElement.querySelector('.lives .value');
-  self.scoreElement = self.gameElement.querySelector('.score .value');
+  self.scoreElement = self.gameElement.querySelector('.score .label');
 
   self.width = self.canvasParentElement.clientWidth;
   self.height = self.canvasParentElement.clientHeight;
@@ -41,4 +39,5 @@ Game.prototype._init = function () {
   self.canvasElement.setAttribute('height', self.height);
 
   self.ctx = self.canvasElement.getContext('2d');
+
 }
